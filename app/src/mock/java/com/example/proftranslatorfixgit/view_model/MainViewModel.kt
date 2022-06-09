b4ed054bc056a3757428fccb8d215f43fc014edc
andrey <a.serbin@litebox.ru>
2022-06-09 21:05:53 +0400

    stubbed MainViewModel with fake data

--- app/src/mock/java/com/example/proftranslatorfixgit/view_model/MainViewModel.kt
+++ app/src/mock/java/com/example/proftranslatorfixgit/view_model/MainViewModel.kt
@@ -3,30 +3,15 @@ package com.example.proftranslatorfixgit.view_model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.model.ApiData
 import com.example.model.AppState
-import com.example.model.Meanings
-import com.example.model.Translation
 import com.example.proftranslatorfixgit.model.ModelProvider
+import com.example.proftranslatorfixgit.view.MockData
 import com.example.utils.parseOnlineSearchResults
 import kotlinx.coroutines.*
 
 class MainViewModel(
     private val provider: ModelProvider
 ) : ViewModel() {
-
-    private val mockApiData: ApiData = ApiData(
-        "September",
-        arrayListOf(
-            Meanings(
-                Translation("\u0441\u0435\u043d\u0442\u044f\u0431\u0440\u044c"),
-                "\\cdn-user77752.skyeng.ru\\resized-images\\640x480\\jpeg\\60\\3c0e381ab5d08df4b6cf9c9e2eb3577a.jpeg",
-                "\\cdn-user77752.skyeng.ru\\resized-images\\640x480\\jpeg\\60\\3c0e381ab5d08df4b6cf9c9e2eb3577a.jpeg"
-            )
-        )
-    )
-    private val mockAppState: AppState = AppState.Success(listOf(mockApiData))
-
     private val liveDataForViewToObserve: MutableLiveData<AppState> = MutableLiveData()
     private fun getPromotions(): LiveData<AppState> = liveDataForViewToObserve
 
@@ -48,7 +33,9 @@ class MainViewModel(
     }
 
     private suspend fun startProvider(word: String) = withContext(Dispatchers.IO) {
-        liveDataForViewToObserve.postValue(parseOnlineSearchResults(provider.getData(word)))
+        liveDataForViewToObserve.postValue(parseOnlineSearchResults(
+            MockData.mockAppState
+        ))
     }
 
     fun getData(word: String) {
